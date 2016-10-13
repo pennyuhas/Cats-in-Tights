f95fca09e1f8c0cb249a5141e1c875ef7bbcfe8c
pennyuhas <130340@bwls.net>
2016-10-13 15:01:37 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
-  createcanvas(800,800)
+  createCanvas(800,800)
 }
 
 function draw() {
-  ellipse(50 , 50 , 110 , 110); ellipse(30, 50, 100, 100);
+  ellipse(100 , 50 , 110 , 110);
 }
