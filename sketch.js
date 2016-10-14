c00be75505218a1595e0d885740f2373d3cfa2b6
pennyuhas <130340@bwls.net>
2016-10-14 15:48:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ function setup() {
 
 function draw() { 
   triangle(mouseX, mouseY, 110, 110);
-  stroke(100,60,0);
+  stroke(75,60,0);
   ellipse(100,100,150,150);
-  fill(100,75,0);
+  fill(80,75,0);
 
 }
