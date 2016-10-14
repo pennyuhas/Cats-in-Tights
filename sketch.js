d7668f4fad9b385f55243cdcbc7a60f4a624f0ff
pennyuhas <130340@bwls.net>
2016-10-14 19:51:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,11 +7,11 @@ function draw() {
   stroke(255,154,52)
   triangle(mouseX, mouseY, 110, 110);
   fill(255,255,0);
-  ellipse(100,110,100,110);
+  ellipse(150,150,150,150);
   stroke(255,255,255);
   
 }
  function mousePressed(){
   fill(255,255,255)
-  ellipse(100,mouseY,150,150);
+  ellipse(mouseX,mouseY,50,50);
 }
