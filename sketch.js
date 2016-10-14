fa31506c44e09afcfb7516dfb10c9f920c006428
pennyuhas <130340@bwls.net>
2016-10-14 15:25:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 function setup() {
   createCanvas(windowWidth , windowHeight)
-  background(90,40,100)
+  background(5,4,100)
 }
 
 function draw() { 
   triangle(mouseX, mouseY, 110, 110);
   stroke(100,5,0);
   ellipse(100,100,150,150);
-  fill(100,20,0);
+  fill(0,100,20,0);
 
 }
