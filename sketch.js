cc6deb69c174233365cbb188c887826af5cbcf5f
pennyuhas <130340@bwls.net>
2016-10-15 10:48:28 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,13 +3,16 @@ function setup() {
   background(5,4,100)
 }
 
+function mousePressed(){
+  fill(255,255,255)
+  ellipse(mouseX,mouseY,50,50);
+}
+
+
 function draw() { 
   fill(255,255,0);
   ellipse(150,150,150,150);
   stroke(255,255,255);
   
 }
- function mousePressed(){
-  fill(255,255,255)
-  ellipse(mouseX,mouseY,50,50);
-}
+
